a0737c1bf981702df6ea464448581c73507f3ac6
Jaedson Silva <contacanal798@gmail.com>
2022-10-01 18:13:56 -0300

    checking host connection and get response time

--- dospy/__main__.py
+++ dospy/__main__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from .dospy import DosPy
 
@@ -10,4 +11,17 @@ def main():
     print('\033[3;33mWARNING: It is your responsibility to use this tool against third-party servers.\033[m\n')
 
     host = input('dospy > Host to attack = ').strip()
-    port = input('dospy > Host port      = ').strip()
+    port = int(input('dospy > Host port      = ').strip())
+
+    dospy = DosPy(host, port)
+
+    print('\nChecking host availability...', end=' ')
+    result = dospy.is_ready_to_connect()
+
+    if result:
+        response_time = dospy.check_connection_ms()
+        print('\033[32mOK\033[m')
+        print(f'Response time is {response_time:.4f}ms')
+    else:
+        print('\033[31mERROR: Could not connect to specified host and port.\033[m')
+        sys.exit(1)
