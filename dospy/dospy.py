23cd1405a9743b65b685a885a53d09446d22ae10
Jaedson Silva <contacanal798@gmail.com>
2022-10-01 17:27:07 -0300

    getting host and port in __init__ method

--- dospy/dospy.py
+++ dospy/dospy.py
@@ -4,15 +4,18 @@ from threading import Thread
 
 
 class DosPy(object):
-    def __init__(self) -> None:
+    def __init__(self, host: str, port: int) -> None:
+        self._host = host
+        self._port = port
         self._threads_num = 100
+
         self._default_protocol = socket.SOCK_DGRAM
 
-    def check_connection_ms(self, host: str, port: int) -> float:
+    def check_connection_ms(self) -> float:
         conn_check = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         start = time.time()
-        conn_check.connect((host, port))
+        conn_check.connect((self._host, self._port))
         end = time.time()
 
         conn_check.close()
